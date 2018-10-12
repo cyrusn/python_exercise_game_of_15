8851751e17c7d1557b56e1f3ee83b131a3a3b7ac
cyrusn <cyrusncy@gmail.com>
2018-10-12 18:20:57 +0800

    use for in loop

--- model/game.py
+++ model/game.py
@@ -2,17 +2,11 @@ class Game:
     """Game store the basic of the game"""
 
     def __init__(self):
-        self.board = [
-            [15, 14, 13, 12],
-            [11, 10, 9, 8],
-            [7, 6, 5, 4],
-            [3, 1, 2, ""]
-        ]
+        self.board = [[15 - 4 * j - i for i in range(4)] for j in range(4)]
+        self.board[3][3] = ''
+        self.board[3][1] = 1
+        self.board[3][2] = 2
         self.blank_loc = [3, 3]
         self.counter = 0
-        self.goal = [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-            [9, 10, 11, 12],
-            [13, 14, 15, ""]
-        ]
+        self.goal = [[i + 4 * j + 1 for i in range(4)] for j in range(4)]
+        self.goal[3][3] = ''
