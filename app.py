ae0b445bb7bdcac88e08b00bc98c2b635c50e9e7
cyrusn <cyrusncy@gmail.com>
2018-10-23 23:36:14 +0800

    add graphic design

--- app.py
+++ app.py
@@ -1,41 +1,121 @@
-import tkinter as tk
-from controller.controller import Controller
-from view.quit import QuitFrame
-from view.message import MessageFrame
-from view.button import ButtonFrame
-from view.grid import GridFrame
+from tkinter import Tk, Frame, Label, CENTER, font
+from game import Game
+
+SIZE = 500
+GRID_LEN = 4
+GRID_PADDING = 10
+BACKGROUND_COLOR_GAME = "#91959B"
+BACKGROUND_COLOR_CELL = "#FFFFFF"
+FONT_COLOR = '#282C34'
+FONT = ("Verdana", 40, "bold")
+
+KEY_QUIT = 'q'
+KEY_RESET = 'r'
+KEY_UP = 'Up'
+KEY_DOWN = 'Down'
+KEY_RIGHT = 'Right'
+KEY_LEFT = 'Left'
 
 
-class App(tk.Frame):
+class App(Frame):
 
     def __init__(self, master):
         super(App, self).__init__()
         self.pack()
+        self.master = master
+        self.game = Game()
+        self.commands = {
+            KEY_QUIT: self.quit,
+            KEY_RESET: self.game.reset,
+            KEY_UP: self.game.move_up,
+            KEY_DOWN: self.game.move_down,
+            KEY_RIGHT: self.game.move_right,
+            KEY_LEFT: self.game.move_left
+        }
+        self.grid_cells = []
+        self.init_grid()
+        self.init_counter_label()
+        self.update_grid()
 
-        master.title("Game of 15")
+        self.window_appearence_setting()
+        self.create_key_event()
 
-        c = Controller()
-        counterVar = tk.StringVar()
-        MessageFrame(counterVar)
-        GridFrame(c, counterVar)
-        QuitFrame()
+    def create_key_event(self):
+        self.master.bind("<Key>", self.listen_key)
 
-        master.lift()
-        master.attributes('-topmost', True)
+    def listen_key(self, e):
+        if e.char in self.commands:
+            self.commands[e.char]()
+
+        if not self.game.win and e.keysym in self.commands:
+            self.commands[e.keysym]()
+            self.update_grid()
+
+    def init_counter_label(self):
+        label_font = font.Font(family='Helvetica', size=24, weight='bold')
+        self.label = Label(
+            self,
+            font=label_font
+        )
+        self.label.grid()
+
+    def init_grid(self):
+        background = Frame(
+            self,
+            bg=BACKGROUND_COLOR_GAME,
+            width=SIZE,
+            height=SIZE
+        )
+        background.grid()
+
+        for row in range(GRID_LEN):
+            row_cells = []
+            for col in range(GRID_LEN):
+                cell = Label(
+                    master=background,
+                    width=SIZE//GRID_LEN,
+                    height=SIZE//GRID_LEN
+                )
+                cell.grid(
+                    column=col,
+                    row=row,
+                    padx=GRID_PADDING,
+                    pady=GRID_PADDING
+                )
+                t = Label(
+                    master=cell,
+                    text='',
+                    bg=BACKGROUND_COLOR_CELL,
+                    justify=CENTER,
+                    font=FONT,
+                    width=4, height=2
+                )
+                t.grid()
+                row_cells.append(t)
+            self.grid_cells.append(row_cells)
+
+    def update_grid(self):
+        for i in range(GRID_LEN):
+            for j in range(GRID_LEN):
+                cell_value = self.game.board[j][i]
+                self.grid_cells[j][i].configure(
+                    text="{}".format(
+                        cell_value if cell_value != 0 else ''
+                    ),
+                    fg=FONT_COLOR
+                )
+        if self.game.win:
+            self.label.configure(
+                text='You win, total steps: {}'.format(self.game.counter))
+        else:
+            self.label.configure(text='Step: {}'.format(self.game.counter))
+
+    def window_appearence_setting(self):
+        self.master.title("Game of 15")
+        self.master.lift()
+        self.master.attributes('-topmost', True)
 
 
-def center_window(width=240, height=180):
-    # get screen width and height
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # calculate position x and y coordinates
-    x = (screen_width / 2) - (width / 2)
-    y = (screen_height / 2) - (height / 2)
-    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
-
-root = tk.Tk()
-center_window()
+root = Tk()
 app = App(root)
 root.mainloop()
---
+++ game.py
@@ -0,0 +1,46 @@
+class Game:
+    def __init__(self):
+        self.reset()
+        self.goal = [[i + 4 * j + 1 for i in range(4)] for j in range(4)]
+        self.goal[3][3] = 0
+
+    def _swap(self, nx=None, ny=None):
+        nx = self.x if nx == None else nx
+        ny = self.y if ny == None else ny
+        x = self.x
+        y = self.y
+        self.board[y][x], self.board[ny][nx] = self.board[ny][nx], self.board[y][x]
+        self.counter += 1
+        self.check_win()
+
+    def move_up(self):
+        if self.y < 3:
+            self._swap(ny=self.y+1)
+            self.y += 1
+
+    def move_down(self):
+        if self.y > 0:
+            self._swap(ny=self.y-1)
+            self.y -= 1
+
+    def move_left(self):
+        if self.x < 3:
+            self._swap(nx=self.x+1)
+            self.x += 1
+
+    def move_right(self):
+        if self.x > 0:
+            self._swap(nx=self.x-1)
+            self.x -= 1
+
+    def check_win(self):
+        if self.board == self.goal:
+            self.win = True
+
+    def reset(self):
+        self.board = [[15 - 4 * j - i for i in range(4)] for j in range(4)]
+        self.board[3][1], self.board[3][2] = self.board[3][2], self.board[3][1]
+        self.x = 3
+        self.y = 3
+        self.counter = 0
+        self.win = False
